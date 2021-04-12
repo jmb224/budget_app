e1c79b83cc0a9f0827034b818268d9bb579ba2dd
Julio Quina <julioquina9@gmail.com>
2021-04-12 15:58:16 +0100

    Update webapck config to ES6

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,33 @@
-const path = require("path");
-const html_webpack_plugin = require("html-webpack-plugin");
+import { resolve } from "path";
+import html_webpack_plugin from "html-webpack-plugin";
 
-module.exports = {
-    entry: ["babel-polyfill/", './src/js/app.js'],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "js/bundle.js"
-    },
-    devServer: {
-        contentBase: "./dist/"
-    },
-    plugins: [
-        new html_webpack_plugin({
-            filename: "index.html",
-            template: "./src/index.html"
-        })
-    ],
-    module: {
-        rules: [
-            {
-               test   : /\.js$/,
-               exclude: /node_modules/,
-               use    : {
-                  loader: "babel-loader"
-               }
+export const entry = ["babel-polyfill/", './src/js/app.js'];
+export const output = {
+    path: resolve(__dirname, 'dist'),
+    filename: "js/bundle.js"
+};
+export const devServer = {
+    contentBase: "./dist/"
+};
+export const plugins = [
+    new html_webpack_plugin({
+        filename: "index.html",
+        template: "./src/index.html"
+    })
+];
+export const module = {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
             }
-        ]
-    },
-    node: {
-        fs: "empty",
-        tls: 'empty',
-        net: 'empty'
-    }
-}
+        }
+    ]
+};
+export const node = {
+    fs: "empty",
+    tls: 'empty',
+    net: 'empty'
+};
